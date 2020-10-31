b713964c750d6fdd974173894614cfbe98f08289
Ian Weyenberg <eeyonw@gmail.com>
2020-10-30 17:45:11 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 cd "$GITHUB_WORKSPACE/${INPUT_DIRECTORY}"
 
-/usr/local/bin/phpcs.phar \
-    --report=summary --report-checkstyle=/tmp/phpcs_result_checkstyle.xml -p -s --colors -d memory_limit=1G --extensions=php \
-    ${INPUT_PHPCS_ARGS:-\.}
+echo "Running Code Sniffer..."
+/usr/local/bin/phpcs.phar${INPUT_PHPCS_ARGS:-\.} > /tmp/phpcs-report.txt
 
 EXIT_CODE1=$?
 
+echo "Running Mess Detector..."
 /usr/local/bin/phpmd.phar ${INPUT_PHPMD_ARGS:-\.} -dmemory_limit=-1 > /tmp/phpmd-report.txt
 
 EXIT_CODE2=$?
