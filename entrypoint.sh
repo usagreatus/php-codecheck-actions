ffa3f8c693592cbf26288647b51fbe12fbc8b6f9
Ian Weyenberg <eeyonw@gmail.com>
2020-10-30 23:08:43 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,13 @@
 cd "$GITHUB_WORKSPACE/${INPUT_DIRECTORY}"
 
 echo "Running Code Sniffer..."
-/usr/local/bin/phpcs.phar ${INPUT_PHPCS_ARGS:-\.} > /tmp/phpcs-report.txt
-cat /tmp/phpcs-report.txt
+/usr/local/bin/phpcs.phar ${INPUT_PHPCS_ARGS:-\.}
 EXIT_CODE1=$?
 echo "Code Sniffer finished. Exit code: ${EXIT_CODE1}"
 
 
 echo "Running Mess Detector..."
-/usr/local/bin/phpmd.phar ${INPUT_PHPMD_ARGS:-\.} > /tmp/phpmd-report.txt
-cat /tmp/phpmd-report.txt
+/usr/local/bin/phpmd.phar ${INPUT_PHPMD_ARGS:-\.} 
 EXIT_CODE2=$?
 echo "Mess Detector finished. Exit code: ${EXIT_CODE2}"
 
