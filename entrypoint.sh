679a29fed9bd9d6955ae378176eb71aeda5c44e8
Ian Weyenberg <eeyonw@gmail.com>
2020-10-31 00:55:48 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "Code Sniffer finished. Exit code: ${EXIT_CODE1}"
 
 echo "Running Mess Detector..."
 /usr/local/bin/phpmd.phar ${INPUT_PHPMD_ARGS:-\.} -dmemory_limit=-1 > /tmp/phpmd-report.txt
-cat /tmp/phpmd-report.txt
+cat /tmp/phpmd-report.txt #exits with 255 if we dont send to file and cat the file
 EXIT_CODE2=$?
 echo "Mess Detector finished. Exit code: ${EXIT_CODE2}"
 
