4e3ce7eef6fac56bddb1ec4a3152d51c7f0de0d9
Ian Weyenberg <eeyonw@gmail.com>
2020-10-30 17:31:31 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,12 @@
 cd "$GITHUB_WORKSPACE/${INPUT_DIRECTORY}"
 
 /usr/local/bin/phpcs.phar \
-    --report=summary \
-    --report-checkstyle=/tmp/phpcs_result_checkstyle.xml \
-    -p -s --colors -d memory_limit=1G --ignore=vendor --extensions=php \
-    --standard=./phpcs.xml \
+    --report=summary --report-checkstyle=/tmp/phpcs_result_checkstyle.xml -p -s --colors -d memory_limit=1G --extensions=php \
     ${INPUT_PHPCS_ARGS:-\.}
-
-cat /tmp/phpcs_result_checkstyle.xml | reviewdog -f=checkstyle -name="phpcs" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 
 EXIT_CODE1=$?
 
-/usr/local/bin/phpmd.phar ${INPUT_PHPMD_ARGS:-\.} text ./phpmd.xml -dmemory_limit=-1 > /tmp/phpmd-report.txt
+/usr/local/bin/phpmd.phar ${INPUT_PHPMD_ARGS:-\.} -dmemory_limit=-1 > /tmp/phpmd-report.txt
 
 EXIT_CODE2=$?
 
